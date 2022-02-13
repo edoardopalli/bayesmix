e531a207fe9ff0a412b9f4456e1b079b91556a2d
edoardopalli <92081440+edoardopalli@users.noreply.github.com>
2022-02-13 23:43:15 +0100

    Cleaning the code

--- src/mixings/MFMMixing.cc
+++ src/mixings/MFMMixing.cc
@@ -17,117 +17,66 @@
 #include <iostream>
 #include <random>
 
-float MFMMixing::factorial(int n) const {
-    if ((n == 0) || (n == 1))
-        return 1;
-    else
-        return n * factorial(n - 1);
-}
+///Note: The value k_max has been considered to be 20.
 
 void MFMMixing::update_K(const std::vector<std::shared_ptr<AbstractHierarchy>> &unique_values) {
 
-  std::cout << "siamo in update_K" << std::endl;
   int kk = get_K_plus();
-  //std::cout << kk << std::endl;
 
   double alpha = exp(state.log_alpha);
   Eigen::VectorXd logprobas(20 - kk + 1);
- // std::cout << logprobas.size() << std::endl;
 
   Eigen::VectorXd log_prior(20 - kk  +1 );
   float val = (float) 1/(20 - kk + 1);
-  //std::cout << val << std::endl;
   for(int i = 0; i < 20 - kk + 1 ; i++){
     log_prior(i) = log(val);
-    //std::cout << 1/(20-kk+1) << std::endl;
-    //std::cout << log_prior(i) << std::endl;
-  }
-  //prova con prior uniforme su K
-
- // std::cout << "Alpha = " << alpha << std::endl;
+    }
 
   auto priorcast = cast_prior();
 
   for(int j = 0; j < logprobas.size(); j++){
 
-    //std::cout << "Iterazione " << j << std::endl;
-
     logprobas(j) = (kk)*log(alpha) + log(std::tgamma(1 + j + kk));
     logprobas(j) -= (kk*log(j+kk) + log(std::tgamma(j + 1)));
-    //std::cout << logprobas(j) << std::endl;
 
     double log_new = 0.0;
     for(int i = 0; i < kk; i++){
 
-      //std::cout << unique_values[i] -> get_card() << std::endl;
-
       long double temp = unique_values[i]->get_card() + (float)alpha/(j+kk);
-
       long double temp2 = std::tgamma(temp);
-      //std::cout << temp2 << std::endl;
       log_new += log(temp2);
       log_new -= log((float)std::tgamma(1 + (float) alpha/(j + kk)));
 
-      //std::cout << log_new << std::endl;
     }
-
-    //std::cout << log_new << std::endl;
-
     logprobas(j) += log_new;
-
-    //std::cout << logprobas(j) << std::endl;
   }
 
   if (priorcast->has_fixed_value()) {
-    std::cout << "Siamo in fixed_values" << std::endl;
     logprobas += log_prior;
-    //std::cout << logprobas << std::endl;
   }
   else if(priorcast -> has_bnb_prior()) {
-    std::cout << "Abbiamo la prior su K" << std::endl;
     double rate = priorcast->bnb_prior().k_prior().rate();
-    std::cout << rate << std::endl;
     double shape_a = priorcast->bnb_prior().k_prior().shape_a();
     double shape_b = priorcast->bnb_prior().k_prior().shape_b();
-    std::cout << shape_b << std::endl;
-    //std::cout << "Abbiamo preso le prior" << std::endl;
 
-    //std::cout << "kk"<<kk << std::endl;
-    //std::cout << "get_k_plus"<< get_K_plus() << std::endl;
-    Eigen::VectorXd priors = bayesmix::evaluate_BNB(rate, shape_a, shape_b, 20);
-    //std::cout << priors << std::endl;
+    Eigen::VectorXd priors = evaluate_BNB(rate, shape_a, shape_b, 20);
 
     for(int i=0; i < 20 - kk +1; i++){
-    std::cout << i << std::endl;
       log_prior(i) = std::log(priors(i+kk-1));
     }
-    std::cout << "SIamo fuori dal for" << std::endl;
     logprobas += log_prior;
   }
 
-  //Devo passare la prior di K
-  //std::cout << "Siamo al categorical rng" << std::endl;
-  //Come input K e K+
   auto &rng = bayesmix::Rng::Instance().get();
   state.K = bayesmix::categorical_rng(stan::math::softmax(logprobas), rng, kk);
 
-  //std::cout << state.K << std::endl;
 }
-
-//void MFMMixing::update_log_alpha() {
-  //Come input K e K+
-  //step 3b
-  //step di metropolis hastings
-//}
 
 void MFMMixing::update_eta(const std::vector<std::shared_ptr<AbstractHierarchy>> &unique_values){
 
-  std::cout << "Siamo all'update etas" << std::endl;
   int K = get_num_components();
   double alpha = exp(state.log_alpha);
   int kk = get_K_plus();
-
-  std::cout << "K: " << K << ", k+: " << kk << std::endl;
 
   Eigen::VectorXd param(K);
   for(int j = 0; j < param.size(); j++){
@@ -138,13 +87,8 @@ void MFMMixing::update_eta(const std::vector<std::shared_ptr<AbstractHierarchy>>
       param(j) = unique_values[j]->get_card();
   }
 
-  //std::cout << param << std::endl;
-
   for(int i = 0; i < K; i++)
     param(i) += (float)(alpha/K);
-
-  //std::cout << param << std::endl;
-
 
   auto &rng = bayesmix::Rng::Instance().get();
   state.etas = stan::math::dirichlet_rng(param,rng);
@@ -153,7 +97,6 @@ void MFMMixing::update_eta(const std::vector<std::shared_ptr<AbstractHierarchy>>
 
 void MFMMixing::update_state(const std::vector<std::shared_ptr<AbstractHierarchy>> &unique_values,
                              const std::vector<unsigned int> &allocations) {
-  std::cout << "Siamo in update state" << std::endl;
 
   update_K(unique_values);
   //update_log_alpha();
@@ -161,57 +104,7 @@ void MFMMixing::update_state(const std::vector<std::shared_ptr<AbstractHierarchy
 
 }
 
-/* Eigen::VectorXd MFMMixing::get_log_etas() {
-  unsigned int K = get_num_components();
-  Eigen::VectorXd log_etas(K);
-  for(int i=0; i<log_etas.size(); i++){
-    log_etas[i] = log(state.etas[i]);
-  }
-
-  return log_etas;
-}
-*/
-
-
-int MFMMixing::sample_BNB() {
-  auto priorcast = cast_prior();
-
-  //Genero rng
-  auto &rng = bayesmix::Rng::Instance().get();
-  //Per sampling da una NB
-  std::default_random_engine generator;
-
-  double rate = priorcast->bnb_prior().k_prior().rate();
-  double shape_a = priorcast->bnb_prior().k_prior().shape_a();
-  double shape_b = priorcast->bnb_prior().k_prior().shape_b();
-  if (rate <= 0) {
-    throw std::invalid_argument("Rate parameter must be > 0");
-  }
-  if (shape_a <= 0) {
-    throw std::invalid_argument("Shape a parameter must be > 0");
-  }
-  if (shape_b <= 0) {
-    throw std::invalid_argument("Shape b parameter must be > 0");
-  }
-
-  //p ~ beta(a,b), p=y1/(y1+y2), y1 ~ gamma(a,d), y2 ~ gamma(b,d)
-  //per ogni d, d=1
-  //p ~ beta(a,b), X ~ negBin(r,p) -> X ~ BNB(r,a,b)
-  //double p = stan::math::beta_rng(rng, shape_a, shape_b);
-  std::gamma_distribution<double> gamma1(shape_a,1);
-  double y1 = gamma1(generator);
-  std::gamma_distribution<double> gamma2(shape_b,1);
-  double y2 = gamma2(generator);
-  double p = y1/(y1+y2);
-  //p è una Beta(rate_a, rate_b)
-
-  std::negative_binomial_distribution<int> BNB(rate,p);
-  return BNB(generator);
-
-}
-
 void MFMMixing::initialize_state() {
-  //std::cout << state.K << std::endl;
   set_K_plus(state.K);
   state.log_alpha = 0;
 
@@ -244,6 +137,7 @@ void MFMMixing::set_state_from_proto(
   state.K = statecast.mfm_state().k();
 }
 
+
 Eigen::VectorXd MFMMixing::mixing_weights(const bool log_, const bool propto) const {
   if(log_){
     Eigen::VectorXd log_etas(state.etas.size());
@@ -255,5 +149,14 @@ Eigen::VectorXd MFMMixing::mixing_weights(const bool log_, const bool propto) co
   else {
     return state.etas;
   }
+
+}
+
+Eigen::VectorXd MFMMixing::evaluate_BNB(int rate, double shape_a, double shape_b, int K_max) {
+    Eigen::VectorXd probas(K_max);
+    for(int i=0; i<K_max; i++){
+        probas[i] = (std::beta(rate + i, shape_a + shape_b)/std::beta(rate,shape_a)) * (tgamma(i + shape_b)/(tgamma(i+1)*tgamma(shape_b)));
+    }
+    return probas;
 
 }
--- src/mixings/MFMMixing.h
+++ src/mixings/MFMMixing.h
@@ -23,33 +23,27 @@
 
 namespace mfm  {
 struct State {
-  int K; double log_alpha; Eigen::VectorXd etas; //occho a gestire alpha/log_alpha nel modo giusto
+  int K; double log_alpha; Eigen::VectorXd etas; //The prior on alpha is for future implementation of a MH step to update it
 };
 }
 
-// Mancano i parametri di input delle funzioni
 class MFMMixing
     : public BaseMixing<MFMMixing, mfm::State, bayesmix::MFMPrior> {
  public:
   MFMMixing() = default;
   ~MFMMixing()= default;
 
-  //NOTA IMPORTANTE: per come è fatta la nostra classe mixing, lo stato è diverso dai paramentri che passiamo come protomessage
-  //quindi le funzioni seguenti vengono overridden solo perchè altrimenti sarebbero pure virtual -> la classe mixing sarebbe così astratta e inutilizzabile
   bool is_conditional() const override { return true; }
+
+  ///Functions to read/write the state of the object on a proto file
   void set_state_from_proto(const google::protobuf::Message &state_) override;
   std::shared_ptr<bayesmix::MixingState> get_state_proto() const override;
 
 
   unsigned int get_num_components() const override {return state.K;}
-//dio merda
-  //double get_log_alpha() {return state.log_alpha;}
-  //Eigen::VectorXd get_etas() {return state.etas; }
-  //Eigen::VectorXd get_log_etas();
-  //void set_etas(Eigen::VectorXd etas_) {state.etas = etas_;}
-  //void set_log_alpha(int log_alpha_) {state.log_alpha = log_alpha_;}
 
-  Eigen::VectorXd mixing_weights(const bool log_,
+    ///mixing_weights() -> overrides a virtual member of AbstractMixing: it returns the mixing weights
+    Eigen::VectorXd mixing_weights(const bool log_,
                                          const bool propto) const override;
 
   bayesmix::MixingId get_id() const override {
@@ -63,22 +57,30 @@ class MFMMixing
 
   void set_K_plus(const int kk) override {K_plus = kk; }
 
-
-  void update_state(const std::vector<std::shared_ptr<AbstractHierarchy>> &unique_values,
+   ///update_state() -> function called by the algorithm object that performs the update of the state of the mixing,
+   ///by running the update of K and of the weights
+   void update_state(const std::vector<std::shared_ptr<AbstractHierarchy>> &unique_values,
                     const std::vector<unsigned int> &allocations) override;
 
+  ///update_K() -> method that samples from the categorical posterior
+  ///the updated value of num_components (elements of the mixture)
   void update_K(const std::vector<std::shared_ptr<AbstractHierarchy>> &unique_values);
 
+  ///update_eta() -> sample new values of the vector of etas (i.e. the weights of the mixture)
+  ///from a finite dirichlet with proper weights
   void update_eta(const std::vector<std::shared_ptr<AbstractHierarchy>> &unique_values);
 
-  //void update_log_alpha();
+  //void update_log_alpha(); to be implemented, for MH step
 
-  float factorial(int n) const;
-
+  ///initialize_state() -> overridden function specific for the initialization of the object mixing
+  ///It initializes K, etas, K_plus = K
   void initialize_state() override;
-  int sample_BNB();
 
- protected:
+  /// Evaluation of a finite dimensional BetaNegativeBinomial PMF
+  Eigen::VectorXd evaluate_BNB(int rate, double shape_a, double shape_b, int K_max);
+
+
+protected:
     int K_plus;
 
 };
